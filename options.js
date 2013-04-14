4d19b7b4bfbaaf7b67cad5b1e899555c92cf7444
Dan Alexander <dan@elearnapp.com>
2013-04-14 13:14:48 -0700

    Refactored Options code

--- options.js
+++ options.js
@@ -1,8 +1,19 @@
 // Howto get background.js page:
 // chrome.extension.getBackgroundPage();
 
+var tips = [
+  'Click on the <img src="icon16.png" /> <b>Read In Bed</b> icon in the Chrome toolbar to enable / disable.',
+  'Press Shift+Ctrl+[Arrow Key] on any webpage to instantly switch orientation.',
+  'You can use Keyboard shortcuts even if ReadInBed is not enabled.'
+];
+
+// Returns a random tip from the array of tips
+function getTip() {
+  return tips[Math.floor(Math.random()*tips.length)];
+}
+
 // Saves options to localStorage.
-function save_options() {
+function save_options(event) {
   
   // Angle setting
   var select = jQuery("input[name='select_angle']").filter(':checked');
@@ -18,10 +29,13 @@ function save_options() {
     showStatus("Options saved");
 
   });
+
+  event.preventDefault();
 }
 
-function showStatus(msg) {
-  $('#status').html(msg).fadeIn().delay(750).fadeOut();
+function showStatus(msg, status_class) {
+  status_class = status_class || "success";
+  $('#status').addClass(status_class).html(msg).slideDown(100, "linear").delay(2000).slideUp(100, "linear", function() { $(this).removeClass(status_class);} )
 }
 
 // Restores select box state to saved value from localStorage.
@@ -36,17 +50,22 @@ function restore_options() {
     }
 
     // Angle
-    //var radiobutton = jQuery("input[name='select_angle'][value="+data.angle+"]");
-    jQuery("#" + data.angle).prop('checked', true);  //jQuery("input[name='select_angle'][value="+data.angle+"]").prop('checked', true);
+    $("#angle_" + data.angle + "_img").click(); // The click event sets the class clicked and checks the radio button, because it is a <label> for the radio
+    //$("#angle_" + data.angle).prop('checked', true);
 
-    // Temporary hack
-    switch(data.angle) {
-      case "90":  click3(); break; 
-      case "180": click2(); break; 
-      case "270": click1(); break; 
-    }
+  });
+  
+  // Draw the tip
+  $('div.tip').html(getTip());
+
+  // Only bind after the page is fully loaded
+  document.querySelector('#save').addEventListener('click', save_options);
+
+  // Adds clicked class to selected img
+  $('div.orientation_list img').click(function() {
+    $('div.orientation_list img').removeClass('clicked');
+    $(this).addClass('clicked');
   });
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
-document.querySelector('#save').addEventListener('click', save_options);
